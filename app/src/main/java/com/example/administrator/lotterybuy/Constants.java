05c2e22fd1367bd00df2c73ba129c3c1b2773e18
1625072810@qq.com <HuangjiaO110.5>
2017-03-14 16:19:40 +0800

    aw

---
+++ app/src/main/java/com/example/administrator/lotterybuy/Constants.java
@@ -0,0 +1,10 @@
+package com.example.administrator.lotterybuy;
+
+/**
+ * 常量类
+ */
+public class Constants {
+    public static final int CONCRETE_A = 1; // A算法
+    public static final int CONCRETE_B = 2; // B算法
+    public static final int CONCRETE_C = 3; // C算法
+}
